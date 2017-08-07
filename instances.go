cd721d95179dcc38f0ea553f860b4bf2ab489ab3
Luke Hines <lukehines@protonmail.com>
2017-08-07 14:05:43 +0100

    Add address keys helper

---
+++ instances.go
@@ -0,0 +1,12 @@
+package manage
+
+const (
+	//FidentInstanceAddressLocal is the address of local fident instance
+	FidentInstanceAddressLocal = "localhost:50052"
+
+	// FidentInstanceAddressSharedLocal is the address of shared internal fident instance
+	FidentInstanceAddressSharedLocal = "accounts.cubex.cloud:50052"
+
+	// FidentInstancePublic is the address of production/public fident instance
+	FidentInstancePublic = "fident.io:50052"
+)
