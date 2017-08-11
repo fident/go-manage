db22ad80533ee097eee41aa824cfd796a5073419
Luke Hines <lukehines@protonmail.com>
2017-08-11 16:42:22 +0100

    Retrieves shared local instance from deckard env vars

--- instances.go
+++ instances.go
@@ -1,12 +1,28 @@
 package manage
 
+import (
+	"fmt"
+	"os"
+)
+
 const (
-	//FidentInstanceAddressLocal is the address of local fident instance
-	FidentInstanceAddressLocal = "localhost:50052"
+	//FidentInstanceAddressLocalHost is the address of local fident instance
+	FidentInstanceAddressLocalHost = "localhost:50052"
 
 	// FidentInstanceAddressSharedLocal is the address of shared internal fident instance
-	FidentInstanceAddressSharedLocal = "accounts.cubex.cloud:50052"
+	FidentInstanceAddressSharedLocal = "HOST$PORT"
 
 	// FidentInstancePublic is the address of production/public fident instance
 	FidentInstancePublic = "fident.io:50052"
+
+	// Deckard env var keys
+	deckardFidentGRPCHostKey = "FIDENT_SERVICE_HOST_GRPC"
+	deckardFidentGRPCPortKey = "FIDENT_SERVICE_PORT_GRPC"
 )
+
+func getAddressFromLocal() string {
+	host := os.Getenv(deckardFidentGRPCHostKey)
+	port := os.Getenv(deckardFidentGRPCPortKey)
+	r := fmt.Sprintf("%s:%s", host, port)
+	return r
+}
