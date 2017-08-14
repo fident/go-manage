4f7de310e034e725f82386388c114b1a7d7fd73b
Luke Hines <lukehines@protonmail.com>
2017-08-14 16:09:09 +0100

    Adds additional management permissions

--- permissions/permissions.go
+++ permissions/permissions.go
@@ -21,4 +21,13 @@ const (
 
 	// PermissionGetAllIdentityIDs is the permission required to read all identity IDs
 	PermissionGetAllIdentityIDs = "fident/management/read/all-identity-ids"
+
+	// PermissionAddManagementPermissions is the permission required to add management permissions to an identity
+	PermissionAddManagementPermissions = "fident/management/write/add-management-permission"
+
+	// PermissionGetManagementPermissions is the permission required to retrieve assigned managemnent permissions for an identity
+	PermissionGetManagementPermissions = "fident/management/read/management-permissions"
+
+	// PermissionRemoveManagementPermissions is the permission required to remove management permissions from an identity
+	PermissionRemoveManagementPermissions = "fident/management/write/remove-management-permission"
 )
