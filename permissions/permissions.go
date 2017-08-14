19168dc238c2eab7bedd02cc9a9ca439b71249f3
Luke Hines <lukehines@protonmail.com>
2017-08-14 11:18:56 +0100

    Updates permissions

--- permissions/permissions.go
+++ permissions/permissions.go
@@ -7,12 +7,18 @@ package permissions
 type entry int
 
 const (
+	// PermissionAll allows account to perform all requests
+	PermissionAll = "fident/management/*"
+
 	// PermissionsReadAll allows account perform all read requests
-	PermissionsReadAll = 800
+	PermissionsReadAll = "fident/management/read"
 
 	// PermissionGetLastLoginTimestamps is the permission required to read login timestamps
-	PermissionGetLastLoginTimestamps = 801
+	PermissionGetLastLoginTimestamps = "fident/management/read/login-timestamps"
 
 	// PermissionGetAccountDetails is the permission required to read account details
-	PermissionGetAccountDetails = 802
+	PermissionGetAccountDetails = "fident/management/read/account-details"
+
+	// PermissionGetAllIdentityIDs is the permission required to read all identity IDs
+	PermissionGetAllIdentityIDs = "fident/management/read/all-identity-ids"
 )
