234fe67bded1be6546c8103817b29a9b61f5bc7e
Luke Hines <lukehines@protonmail.com>
2017-11-13 15:32:23 +0000

    Adds user account creation

---
+++ accountCreate.go
@@ -0,0 +1,53 @@
+package manage
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/fident/go-manage/tls"
+
+	"github.com/fident/go-proto/fident"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+)
+
+// CreateUserAccount creates a new user account returning identity ID and optionally a temporary session cookie
+func (i *Instance) CreateUserAccount(email string, tempSessionCookie bool) (string, *http.Cookie, error) {
+	meta, err := i.preflightAuth()
+	if err != nil {
+		return "", nil, err
+	}
+	ctx := metadata.NewOutgoingContext(context.Background(), meta)
+
+	conn, err := grpc.Dial(i.fidentEndpoint, grpc.WithTransportCredentials(credentials.NewTLS(tls.FidentTSLConfig)))
+	if err != nil {
+		return "", nil, err
+	}
+	defer conn.Close()
+
+	c := fident.NewAuthClient(conn)
+	res, err := c.CreateUserAccount(ctx, &fident.CreateUserAccountRequest{
+		EmailAddress:        email,
+		IssueTemporaryToken: tempSessionCookie,
+	})
+	if err != nil {
+		return "", nil, err
+	}
+
+	var cookie *http.Cookie
+
+	if tempSessionCookie {
+		cookie = &http.Cookie{
+			Name:     res.TemporaryToken.Name,
+			Value:    res.TemporaryToken.Value,
+			Domain:   res.TemporaryToken.Domain,
+			Expires:  time.Unix(res.TemporaryToken.ExpiresEpoch, 0),
+			Secure:   res.TemporaryToken.Secure,
+			HttpOnly: res.TemporaryToken.HttpOnly,
+		}
+	}
+
+	return res.IdentityId, cookie, err
+}
--- example/main.go
+++ example/main.go
@@ -7,32 +7,16 @@ import (
 )
 
 const (
-	keyfilePath = "./testkey.json"
+	keyfilePath = "./cubex_cloud_testkey.json"
 )
 
 func main() {
 	// Create new fident client with path to keyfile.json and fident instance address
 	// (Note you can use 'client.FidentInstanceAddressSharedLocal' variable to connect using deckard env vars)
-	testClient, err := client.New("./cubex_cloud_testkey.json", client.FidentInstanceAddressSharedLocal)
+	testClient, err := client.New(keyfilePath, client.FidentInstanceAddressSharedLocal)
 	if err != nil {
 		panic(err)
 	}
-
-	// Query account details using client
-	details, err := testClient.GetAccountDetailsForIdentityID("EFIDFIID-ZGVT5I6L4-MISCR-V5UX35S")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Details Result: %v\n", details)
-
-	// Query last login timestamp using client
-	lastlogin, err := testClient.GetLastLoginTimestampForIdentityID("EFIDFIID-ZGVT5I6L4-MISCR-V5UX35S")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Timestamp Result: %v\n", lastlogin)
 
 	// Query all identity ids for project
 	ids, err := testClient.GetAllIdentityIDsForProject()
@@ -42,8 +26,41 @@ func main() {
 
 	fmt.Printf("All IDs Result: %v\n", ids)
 
-	/*err = testClient.AddManagementPermission("EFIDFIID-ZGTMFQ1EO-MISCR-IAPZNMF", permissions.PermissionAll)
-	if err != nil {
-		panic(err)
-	}*/
+	// Query account details using client
+	/*
+		details, err := testClient.GetAccountDetailsForIdentityID("EFIDFIID-ZGVT5I6L4-MISCR-V5UX35S")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Details Result: %v\n", details)
+	*/
+
+	// Query last login timestamp using client
+	/*
+		lastlogin, err := testClient.GetLastLoginTimestampForIdentityID("EFIDFIID-ZGVT5I6L4-MISCR-V5UX35S")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Timestamp Result: %v\n", lastlogin)
+	*/
+
+	// Adding management permission example
+	/*
+		err = testClient.AddManagementPermission("EFIDFIID-ZGTMFQ1EO-MISCR-IAPZNMF", permissions.PermissionAll)
+		if err != nil {
+			panic(err)
+		}
+	*/
+
+	// Creating user account example
+	/*
+		id, _, err := testClient.CreateUserAccount("tetcom", false)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Created account with ID: %v\n", id)
+	*/
 }
